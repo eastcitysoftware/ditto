1de3bc150f6d6948ca17fa10793e6c54368b2573
Pim Brouwers <pimnation@gmail.com>
2025-12-12 05:45:18 -0500

    page collection factory

--- src/Ditto/src/PageCollection.cs
+++ src/Ditto/src/PageCollection.cs
@@ -1,45 +1,31 @@
-// namespace Ditto;
+namespace Ditto;
 
-// public sealed class PageCollection {
-//     private readonly Dictionary<string, IReadOnlyList<PageInfo>> _pages = [];
+public sealed record PageCollection(
+    string Name,
+    IReadOnlyList<Page> Pages);
 
-//     internal PageCollection(Dictionary<string, List<PageInfo>> pages) {
-//         _pages = pages.ToDictionary(
-//             kvp => kvp.Key,
-//             kvp => (IReadOnlyList<PageInfo>)kvp.Value.AsReadOnly(),
-//             StringComparer.OrdinalIgnoreCase);
-//     }
+public static class PageCollectionFactory {
+    public static IEnumerable<PageCollection> Create(IEnumerable<Page> pages) {
+        var pageDict = new Dictionary<string, List<Page>>();
 
-//     public static PageCollection Empty => new([]);
+        // collections are derived from the first segment of the url
+        // only pages with n > 1 subpages are included in collections
+        foreach (var page in pages) {
+            if (page.Path.Split('/', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries) is string[] segments
+                && segments.Length > 1) {
+                var collectionName = segments[0];
 
-//     public IReadOnlyList<PageInfo> this[string collection] =>
-//         _pages.TryGetValue(collection, out var pages) ? pages : [];
-// }
+                if (pageDict.TryGetValue(collectionName, out var value)) {
+                    pageDict[collectionName].Add(page);
+                }
+                else {
+                    pageDict[collectionName] = [page];
+                }
+            }
+        }
 
-// public interface IPageCollectionProcessor {
-//     PageCollection Process(IEnumerable<PageInfo> pages);
-// }
-
-// public sealed class PageCollectionProcessor : IPageCollectionProcessor {
-//     public PageCollection Process(IEnumerable<PageInfo> pages) {
-//         var pageDict = new Dictionary<string, List<PageInfo>>();
-
-//         // collections are derived from the first segment of the url
-//         // only pages with n > 1 subpages are included in collections
-//         foreach (var page in pages) {
-//             if (page.Path.Split('/', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries) is string[] segments
-//                 && segments.Length > 1) {
-//                 var collectionName = segments[0];
-
-//                 if (pageDict.TryGetValue(collectionName, out var value)) {
-//                     pageDict[collectionName].Add(page);
-//                 }
-//                 else {
-//                     pageDict[collectionName] = [page];
-//                 }
-//             }
-//         }
-
-//         return new(pageDict);
-//     }
-// }
+        foreach (var kvp in pageDict) {
+            yield return new(kvp.Key, kvp.Value.AsReadOnly());
+        }
+    }
+}
